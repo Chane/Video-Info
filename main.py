6f8b9e80a7b13d04dc0516e684d60eba414bf215
Nikolai Dante <git@nathwilson.co.uk>
2023-01-17 16:12:33 +0000

    First version

---
+++ main.py
@@ -0,0 +1,5 @@
+from videoInfo import VideoInfo
+
+if __name__ == "__main__":
+    cc = VideoInfo()
+    cc.run()
---
+++ videoInfo.py
@@ -0,0 +1,59 @@
+import logging
+import sys
+import argparse
+import os
+
+from config import VIDEOFOLDER
+from videoprops import get_video_properties
+
+class VideoInfo:
+    def __init__(self):
+        parser = argparse.ArgumentParser();
+        parser.add_argument(
+            '-d', '--debug',
+            help = "Log debug information",
+            action = "store_const", dest = "loglevel", const = logging.DEBUG,
+            default = logging.WARNING
+        )
+
+        parser.add_argument(
+            '-v', '--verbose',
+            help = "Log verbose information",
+            action = "store_const", dest = "loglevel", const = logging.INFO
+        )
+
+        args = parser.parse_args();
+
+        logging.basicConfig(stream = sys.stdout, level = args.loglevel)
+        self.logger = logging.getLogger(__name__)
+
+    def writeMessage(self, message):
+        self.logger.log(logging.INFO, message)
+
+    def writeDebug(self, message):
+        self.logger.log(logging.DEBUG, message)
+
+    def run(self):
+        self.writeMessage(VIDEOFOLDER)
+
+        info = []
+        for root, dirs, files in os.walk(VIDEOFOLDER):
+            for file in files:
+                file_path = VIDEOFOLDER + '/' + file
+                props = get_video_properties(file_path)
+
+                file_stats = os.stat(file_path)
+                size = file_stats.st_size
+
+                file_size = round((size / 1024) / 1014, 2)
+
+                clean_file = file.replace(',','')
+
+                detail = f'''{clean_file},{props['width']},{props['height']},{props['codec_name']},{file_size}'''
+                self.writeDebug(detail)
+
+                info.append(detail)
+        
+        with open (os.path.dirname(os.path.abspath(__file__)) + '/info.csv', 'w') as f:
+            for line in info:
+                f.write(f"{line}\n")
